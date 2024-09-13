f4a857df5044e485e709c4b091eb53485ad1cb27
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-13 19:31:44 +0530

    update,delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -47,8 +47,81 @@ const addUser = async (req, res) => {
   });
 };
 
+const deleteUser = async (req, res) => {
+  //get id from req.params object...
+  const id = req.params.id;
+  const deletedUser = await userSchema.findByIdAndDelete(id);
+  console.log("deleted user...", deletedUser);
+
+  if (deletedUser === null) {
+    res.status(404).json({
+      message: "User Not Found !!",
+      data: {},
+    });
+  } else {
+    res.status(200).json({
+      message: "User Deleted Successfully !!",
+      data: deletedUser,
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  //update user set name=?,email=? where id=?
+  //id -->req.params.id
+  //name,email -->req.body
+
+  const id = req.params.id;
+  const dataToUpdate = req.body;
+  //console.log("data to update...", dataToUpdate);
+
+  const updatedUser = await userSchema.findByIdAndUpdate(id, dataToUpdate,{new:true});
+  if(updatedUser || updatedUser!=null){
+    res.status(200).json({
+      message: "User Updated Successfully !!",
+      data: updatedUser,
+    });
+  }
+    else{
+      res.status(404).json({
+        message: "User Not Found !!",
+        data: {},
+      });
+    }
+  //console.log("updated user...", updatedUser);
+  // res.status(200).json({
+  //   message: "User Updated Successfully !!",
+  //   data: updatedUser,
+  // });
+};
+
+const deleteByAge = async (req, res) => {
+
+  const age = req.query.age;
+  const deletedUsers = await userSchema.deleteMany({age:{$gte:age}});
+  console.log("deleted users...", deletedUsers);
+  if (deletedUsers.deletedCount > 0) {
+    res.status(200).json({
+      message: "Users Deleted Successfully !!",
+      data: deletedUsers,
+    });
+  } else {
+    res.status(404).json({
+      message: "Users Not Found !!",
+      data: {},
+    });
+  }
+
+
+
+}
+
+
 module.exports = {
   getAllUsers,
   getUserById,
   addUser,
+  deleteUser,
+  updateUser,
+  deleteByAge
 };
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -5,4 +5,7 @@ router.get("/users",userController.getAllUsers)
 router.get("/user/:id",userController.getUserById)
 //localhost:3001/user/adduser
 router.post("/adduser",userController.addUser)
+router.delete("/deleteuser/:id",userController.deleteUser)
+router.put("/updateuser/:id",userController.updateUser)
+router.delete("/deletebyage",userController.deleteByAge)
 module.exports = router;
